2e61729761cf02939169720dc377a673d83a1c1f
sumukhahm2 <sumukhahm2@gmail.com>
2024-04-30 20:43:17 +0530

    request-and -response

--- app.js
+++ app.js
@@ -1,7 +1,37 @@
 const http=require('http');
 
 const server=http.createServer((req,res)=>{
-  console.log('vighnaraj H M')
+ const url=req.url
+ if(url=='/home')
+ {
+    res.write('<html>')
+  res.write('<head><title>My First Page</title></head>')
+  res.write('<body><h1>Welcome to Home</h1></body>')
+  res.write('</html>')
+  return res.end()
+ }
+ if(url=='/about')
+ {
+    res.write('<html>')
+  res.write('<head><title>My First Page</title></head>')
+  res.write('<body><h1>Welcome About Us</h1></body>')
+  res.write('</html>')
+  return res.end()
+ }
+ if(url=='/node')
+ {
+    res.write('<html>')
+  res.write('<head><title>My First Page</title></head>')
+  res.write('<body><h1>Welcome to Node JS</h1></body>')
+  res.write('</html>')
+  return res.end()
+ }
+  res.setHeader('Content-Type','text/html')
+  res.write('<html>')
+  res.write('<head><title>My First Page</title></head>')
+  res.write('<body><h1>Hello How Are You</h1></body>')
+  res.write('</html>')
+  res.end()
 });
 
 server.listen(4000)
