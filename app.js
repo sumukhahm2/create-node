fc92a542bd37df30751ba67d50b5df1b5d021149
sumukhahm2 <sumukhahm2@gmail.com>
2024-05-03 10:39:28 +0530

    event-driven-code-execution

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const http=require('http');
+const fs=require('fs')
+
 
 const server=http.createServer((req,res)=>{
  const url=req.url
+ const method=req.method
  if(url=='/home')
  {
     res.write('<html>')
@@ -10,14 +13,19 @@ const server=http.createServer((req,res)=>{
   res.write('</html>')
   return res.end()
  }
- if(url=='/about')
+
+ 
+ if(url=='/')
  {
+    fs.readFile("message.txt",{encoding:'utf-8'},(err,data)=>{
     res.write('<html>')
-  res.write('<head><title>My First Page</title></head>')
-  res.write('<body><h1>Welcome About Us</h1></body>')
+  res.write('<head><title>Enter Message</title></head>')
+  res.write(`<body>${data}</body>`)
+  res.write('<body><form action="/message" method="POST"><input type="text" name="message"/><button>Click</button</form></body>')
   res.write('</html>')
   return res.end()
- }
+ })
+}
  if(url=='/node')
  {
     res.write('<html>')
@@ -26,12 +34,32 @@ const server=http.createServer((req,res)=>{
   res.write('</html>')
   return res.end()
  }
-  res.setHeader('Content-Type','text/html')
+ if(url=='/message' && method=='POST')
+ {
+    const body=[]
+    req.on('data',(chunk)=>{
+        body.push(chunk)
+    })
+
+    req.on('end',()=>{
+        parseBody=Buffer.concat(body).toString()
+        const data=parseBody.split("=")[1]
+        fs.writeFileSync('message.txt',data)
+        res.statusCode=302
+        res.setHeader('Location','/')
+       return res.end()
+    })
+    
+  
+ }
+
+  /*res.setHeader('Content-Type','text/html')
   res.write('<html>')
   res.write('<head><title>My First Page</title></head>')
   res.write('<body><h1>Hello How Are You</h1></body>')
   res.write('</html>')
-  res.end()
+  res.end()*/
+ 
 });
 
 server.listen(4000)
