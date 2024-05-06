3c530941b4e902db9050b98f20a581beaeab0151
sumukhahm2 <sumukhahm2@gmail.com>
2024-05-06 08:57:14 +0530

    app-listen

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 const http=require('http');
 //const fs=require('fs')
+const express=require('express')
 
-const routes=require('./routes')
-console.log(routes)
-const server=http.createServer(routes);
+const app=express()
 
-server.listen(3000,function(error){
-    if(error)
-    {
-       console.log(error)
-    }
-    else{
-       console.log('success')
-    }
+app.use((req,res,next)=>{
+   console.log('in the middleware') 
+   next()
 })
+app.use((req,res,next)=>{
+    console.log('in other middleware') 
+    //res.send('<h1>Hello Express</h1>')
+    res.send( { key1: 'value' })
+ })
+
+ app.listen(3000)
