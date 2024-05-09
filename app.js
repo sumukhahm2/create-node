8224271efa701549d23544778d28e54479f03025
sumukhahm2 <sumukhahm2@gmail.com>
2024-05-09 09:21:52 +0530

    routing-in -middleware

--- app.js
+++ app.js
@@ -1,17 +1,30 @@
 const http=require('http');
 //const fs=require('fs')
 const express=require('express')
-
+const bodyParser=require('body-parser')
 const app=express()
 
-app.use((req,res,next)=>{
-   console.log('in the middleware') 
-   next()
+
+app.use(bodyParser.urlencoded({extended:false}))
+app.use('/add-product',(req,res,next)=>{
+   console.log('in other middleware') 
+   //res.send('<h1>Hello Express</h1>')
+   res.send('<form action="/product" method="POST"><input type="text" name="product"/><button>Add Product</button></form><form action="/product" method="POST"><input type="text" name="product"/><button>Add Product</button></form>')
+   
 })
-app.use((req,res,next)=>{
-    console.log('in other middleware') 
-    //res.send('<h1>Hello Express</h1>')
-    res.send( { key1: 'value' })
- })
+
+app.post('/product',(req,res,next)=>{
+   console.log(req.body)
+   
+   
+})
+
+
+app.use('/',(req,res,next)=>{
+   console.log('in the middleware') 
+   res.send('<h1>Hello Express</h1>')
+   
+})
+
 
  app.listen(3000)
